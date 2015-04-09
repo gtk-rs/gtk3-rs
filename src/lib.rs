3d5f963ff3c38afbfd8ccad263a94b97443e3d93
Gleb Kozyrev <gleb@gkoz.com>
2015-04-09 17:51:39 +0300

    gdk: Cargo.toml cleanup and some renaming

--- src/lib.rs
+++ src/lib.rs
@@ -21,7 +21,7 @@ Bindings and wrappers for __GDK__
 
 #![feature(unique)]
 
-extern crate gdk3_sys as gdk_ffi;
+extern crate gdk_sys as gdk_ffi;
 extern crate glib_sys as glib_ffi;
 extern crate glib as glib_main;
 extern crate libc;
