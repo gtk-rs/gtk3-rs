529bfe6087d76b2c542efe5953d3365e2cc261cd
Gleb Kozyrev <gleb@gkoz.com>
2015-04-16 19:27:19 +0300

    gdk: Update license headers

--- src/macros.rs
+++ src/macros.rs
@@ -1,6 +1,7 @@
 // 
-// This file is part of the rust-gnome project
-// 
+// Copyright 2013-2015, The Rust-GNOME Project Developers.
+// See the COPYRIGHT file at the top-level directory of this distribution.
+// Licensed under the MIT license, see the LICENSE file or <http://opensource.org/licenses/MIT>
 
 #![macro_use]
 
