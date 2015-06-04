97810eddeb5ddf863fb0c2885857845459a54cf9
Guillaume Gomez <guillaume1.gomez@gmail.com>
2015-06-04 16:14:59 +0200

    glib: Add documentation

--- src/error.rs
+++ src/error.rs
@@ -6,11 +6,15 @@ use ffi::{self, GQuark};
 use glib_container::GlibContainer;
 use translate::ToGlibPtr;
 
+/// The Error structure contains information about an error that has occurred.
 pub struct Error {
     pointer: *mut ffi::GError
 }
 
 impl Error {
+    /// Creates a new GError; unlike g_error_new(), message is not a printf()-style format string.
+    /// Use this function if message contains text you don't have control over, that could include
+    /// printf() escape sequences.
     pub fn new_literal(domain: GQuark, code: i32, message: &str) -> Option<Error> {
         let tmp_pointer = unsafe {
             ffi::g_error_new_literal(domain, code, message.to_glib_none().0)
@@ -23,6 +27,7 @@ impl Error {
         }
     }
 
+    /// Frees a GError and associated resources.
     pub fn release(&mut self) -> () {
         if !self.pointer.is_null() {
             unsafe { ffi::g_error_free(self.pointer) };
@@ -30,6 +35,13 @@ impl Error {
         }
     }
 
+    /// Returns true if error matches domain and code , false otherwise. In particular, when error
+    /// is NULL, false will be returned.
+    /// 
+    /// If domain contains a FAILED (or otherwise generic) error code, you should generally not check
+    /// for it explicitly, but should instead treat any not-explicitly-recognized error code as being
+    /// equivalent to the FAILED code. This way, if the domain is extended in the future to provide a
+    /// more specific error code for a certain case, your code will still work.
     pub fn matches(&self, domain: GQuark, code: i32) -> bool {
         match unsafe { ffi::g_error_matches(self.pointer, domain, code) } {
             ffi::GFALSE => false,
@@ -37,12 +49,19 @@ impl Error {
         }
     }
 
+    /// Does nothing if self.pointer is NULL; if self.pointer is non-NULL, then *self.pointer must be NULL.
+    /// A new GError is created and assigned to *self.pointer .
     pub fn set(&mut self, domain: GQuark, code: i32, message: &str) -> () {
         unsafe {
             ffi::g_set_error_literal(&mut self.pointer, domain, code, message.to_glib_none().0)
         }
     }
 
+    /// If other.pointer is NULL, free src ; otherwise, moves src into *other . The error variable
+    /// other.pointer points to must be NULL.
+    /// 
+    /// Note that src is no longer valid after this call. If you want to keep using the same GError*, you
+    /// need to set it to NULL after calling this function on it.
     pub fn propagate(&mut self, other: &Error) -> () {
         unsafe { ffi::g_propagate_error(&mut self.pointer, other.pointer) }
     }
