186aa5ec0452103599a9c00ae9ace8146a504e8d
Guillaume Gomez <guillaume1.gomez@gmail.com>
2016-03-10 12:26:23 +0100

    examples: Clicking on a row now updates a label

--- src/simple_treeview.rs
+++ src/simple_treeview.rs
@@ -1,24 +1,46 @@
 //! # TreeView Sample
 //!
-//! This sample demonstrates how to create a TreeView with either a ListStore or TreeStore.
+//! This sample demonstrates how to create a TreeView with a ListStore.
 
-extern crate glib;
 extern crate gtk;
 
 use gtk::prelude::*;
 use gtk::{
-    CellRendererText, TreeStore, TreeView, TreeViewColumn,
+    CellRendererText, Label, ListStore, Orientation, TreeView, TreeViewColumn,
     Window, WindowPosition, WindowType
 };
+
+fn create_and_fill_model() -> ListStore {
+    // Creation of a model with two rows.
+    let model = ListStore::new(&[u32::static_type(), String::static_type()]);
+
+    // Filling up the tree view.
+    let entries = &["Michel", "Sara", "Liam", "Zelda", "Neo", "Octopus master"];
+    for (i, entry) in entries.iter().enumerate() {
+        model.insert_with_values(None, &[0, 1], &[&(i as u32 + 1), &entry]);
+    }
+    model
+}
 
 fn append_column(tree: &TreeView, id: i32) {
     let column = TreeViewColumn::new();
     let cell = CellRendererText::new();
 
     column.pack_start(&cell, true);
-    // we create the column at the position `id`
+    // Association of the view's column with the model's `id` column.
     column.add_attribute(&cell, "text", id);
     tree.append_column(&column);
+}
+
+fn create_and_setup_view() -> TreeView {
+    // Creating the tree view.
+    let tree = TreeView::new();
+
+    tree.set_headers_visible(false);
+    // Creating the two columns inside the view.
+    append_column(&tree, 0);
+    append_column(&tree, 1);
+    tree
 }
 
 fn main() {
@@ -37,40 +59,39 @@ fn main() {
         Inhibit(false)
     });
 
-    // we create a view
-    let tree = TreeView::new();
-    // we create a model with two rows
-    let store = TreeStore::new(&[String::static_type(), i32::static_type()]);
+    // Creating a vertical layout to place both tree view and label in the window.
+    let vertical_layout = gtk::Box::new(Orientation::Vertical, 0);
 
-    // when a line is selected, this signal is called
+    // Creation of the label.
+    let label = Label::new(None);
+
+    let tree = create_and_setup_view();
+
+    let model = create_and_fill_model();
+    // Setting the model into the view.
+    tree.set_model(Some(&model));
+    // Adding the view to the layout.
+    vertical_layout.add(&tree);
+    // Same goes for the label.
+    vertical_layout.add(&label);
+
+    // The closure responds to selection changes by connection to "::cursor-changed" signal,
+    // that gets emitted when the cursor moves (focus changes).
     tree.connect_cursor_changed(move |tree_view| {
         let selection = tree_view.get_selection();
         if let Some((model, iter)) = selection.get_selected() {
-            // We are now getting back the values from the line corresponding to the
+            // Now getting back the values from the row corresponding to the
             // iterator `iter`.
             //
             // The `get_value` method do the conversion between the gtk type and Rust.
-            println!("Hello '{}' from line {}",
-                     model.get_value(&iter, 0).get::<String>().unwrap(),
-                     model.get_value(&iter, 1).get::<i32>().unwrap() + 1);
+            label.set_text(&format!("Hello '{}' from row {}",
+                                    model.get_value(&iter, 1).get::<String>().unwrap(),
+                                    model.get_value(&iter, 0).get::<u32>().unwrap()));
         }
     });
 
-    // we set the model into the view
-    tree.set_model(Some(&store));
-    tree.set_headers_visible(false);
-    // we create the two columns inside the view
-    append_column(&tree, 0);
-    append_column(&tree, 1);
-
-    // we fill the treeview
-    let entries = &["Michel", "Sara", "Liam", "Zelda", "Neo", "Octopus master"];
-    for (i, entry) in entries.iter().enumerate() {
-        store.insert_with_values(None, None, &[0, 1], &[&entry, &(i as i32)]);
-    }
-
-    // we add the view to the window
-    window.add(&tree);
+    // Adding the layout to the window.
+    window.add(&vertical_layout);
 
     window.show_all();
     gtk::main();
