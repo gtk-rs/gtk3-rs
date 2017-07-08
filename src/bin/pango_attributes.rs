e87e8f556f677dbadc8bcd41b1f92470de69abed
Evgenii Pashkin <eapashkin@gmail.com>
2017-07-08 20:31:21 +0300

    examples: Add example for pango text attributes

---
+++ src/bin/pango_attributes.rs
@@ -0,0 +1,56 @@
+//! # Pango text attributes
+//!
+//! This sample demonstrates how to use various attributes on labels text.
+
+#![crate_type = "bin"]
+
+extern crate gtk;
+extern crate pango;
+
+use gtk::prelude::*;
+
+fn main() {
+    if gtk::init().is_err() {
+        println!("Failed to initialize GTK.");
+        return;
+    }
+
+    let window = gtk::Window::new(gtk::WindowType::Toplevel);
+
+    window.set_title("Pango text attributes");
+    window.set_border_width(10);
+    window.set_position(gtk::WindowPosition::Center);
+    window.set_default_size(350, 70);
+
+    window.connect_delete_event(|_, _| {
+        gtk::main_quit();
+        Inhibit(false)
+    });
+
+    let label = gtk::Label::new("Some text");
+    let attr_list = pango::AttrList::new();
+
+    let mut attr = pango::Attribute::new_background(65535, 0, 0).unwrap();
+    attr.set_start_index(0);
+    attr.set_end_index(2);
+    attr_list.insert(attr);
+
+    let mut attr = pango::Attribute::new_underline(pango::Underline::Single).unwrap();
+    attr.set_start_index(1);
+    attr.set_end_index(4);
+    attr_list.insert(attr);
+
+    let mut attr = pango::Attribute::new_strikethrough(true).unwrap();
+    attr.set_start_index(5);
+    attr_list.insert(attr);
+
+    let mut attr = pango::Attribute::new_scale(1.2).unwrap();
+    attr.set_start_index(6);
+    attr_list.insert(attr);
+
+    label.set_attributes(&attr_list);
+    window.add(&label);
+
+    window.show_all();
+    gtk::main();
+}
