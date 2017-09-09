e3956ddacfb4ba806524e64593fcaf007ad72823
Sebastian Dröge <sebastian@centricular.com>
2017-09-09 15:33:29 +0300

    gio: Regenerate with latest GIR

--- src/application.rs
+++ src/application.rs
@@ -4,7 +4,7 @@ use ffi;
 use glib;
 use glib::object::Downcast;
 use glib::object::IsA;
-use glib::signal::connect;
+use glib::signal::{SignalHandlerId, connect};
 use glib::translate::*;
 use glib_ffi;
 use libc;
@@ -12,11 +12,11 @@ use std::boxed::Box as Box_;
 use std::mem::transmute;
 
 pub trait ApplicationExtManual {
-    fn connect_open<F: Fn(&Self, &[File], &str) + 'static>(&self, f: F) -> u64;
+    fn connect_open<F: Fn(&Self, &[File], &str) + 'static>(&self, f: F) -> SignalHandlerId;
 }
 
 impl<O: IsA<Application> + IsA<glib::object::Object>> ApplicationExtManual for O {
-    fn connect_open<F: Fn(&Self, &[File], &str) + 'static>(&self, f: F) -> u64 {
+    fn connect_open<F: Fn(&Self, &[File], &str) + 'static>(&self, f: F) -> SignalHandlerId {
         unsafe {
             let f: Box_<Box_<Fn(&Self, &[File], &str) + 'static>> = Box_::new(Box_::new(f));
             connect(self.to_glib_none().0, "open",
