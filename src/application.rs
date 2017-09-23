6d9a19ce5deab3e0ef70def1c199ac798f126557
Guillaume Gomez <guillaume1.gomez@gmail.com>
2017-09-23 14:41:49 +0200

    gio: Update run method

--- src/application.rs
+++ src/application.rs
@@ -12,10 +12,18 @@ use std::boxed::Box as Box_;
 use std::mem::transmute;
 
 pub trait ApplicationExtManual {
+    fn run(&self, argv: &[String]) -> i32;
     fn connect_open<F: Fn(&Self, &[File], &str) + 'static>(&self, f: F) -> SignalHandlerId;
 }
 
 impl<O: IsA<Application> + IsA<glib::object::Object>> ApplicationExtManual for O {
+    fn run(&self, argv: &[String]) -> i32 {
+        let argc = argv.len() as i32;
+        unsafe {
+            ffi::g_application_run(self.to_glib_none().0, argc, argv.to_glib_none().0)
+        }
+    }
+
     fn connect_open<F: Fn(&Self, &[File], &str) + 'static>(&self, f: F) -> SignalHandlerId {
         unsafe {
             let f: Box_<Box_<Fn(&Self, &[File], &str) + 'static>> = Box_::new(Box_::new(f));
