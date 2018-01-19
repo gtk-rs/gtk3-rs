47aae4ddc9eb2fdcdc4be12b416e1f898d4f1f76
Guillaume Gomez <guillaume1.gomez@gmail.com>
2018-01-19 22:13:44 +0100

    gdk: regen

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,8 @@
-// Copyright 2013-2015, The Gtk-rs Project Developers.
+// Copyright 2013-2018, The Gtk-rs Project Developers.
 // See the COPYRIGHT file at the top-level directory of this distribution.
 // Licensed under the MIT license, see the LICENSE file or <http://opensource.org/licenses/MIT>
+
+#![allow(deprecated)]
 
 extern crate glib_sys as glib_ffi;
 extern crate gdk_sys as ffi;
