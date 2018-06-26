0591491813073b1487f40880a3eaea30adad4a97
Guillaume Gomez <guillaume1.gomez@gmail.com>
2018-06-26 23:53:07 +0200

    gio: Small improvements to avoid warnings and add a bit of doc

--- src/subprocess_launcher.rs
+++ src/subprocess_launcher.rs
@@ -1,14 +1,17 @@
 #[cfg(any(feature = "v2_40", feature = "dox"))]
 use SubprocessLauncher;
-#[cfg(any(feature = "v2_40", feature = "dox"))]
+#[cfg(any(all(feature = "v2_40", not(windows)), feature = "dox"))]
 use ffi;
 #[cfg(any(feature = "v2_40", feature = "dox"))]
 use glib;
 #[cfg(any(feature = "v2_40", feature = "dox"))]
 use glib::object::IsA;
-#[cfg(any(all(feature = "v2_40", not(windows)), all(feature = "dox", not(windows))))]
+#[cfg(any(all(feature = "v2_40", not(windows)), all(feature = "dox", unix)))]
 use std::os::unix::io::IntoRawFd;
-#[cfg(all(feature = "dox", windows))]
+#[cfg(all(feature = "dox", not(unix)))]
+/// Replacement for "real" [`IntoRawFd`] trait for non-unix targets.
+///
+/// [`IntoRawFd`]: https://doc.rust-lang.org/std/os/unix/io/trait.IntoRawFd.html
 pub trait IntoRawFd {}
 
 pub trait SubprocessLauncherExtManual {
