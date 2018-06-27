663d4e24022cd8af6e6bb4278d5305d6c6a6f71e
Evgenii Pashkin <eapashkin@gmail.com>
2018-06-27 20:33:57 +0300

    gio: Ignore clippy warnings

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,9 @@
 // Licensed under the MIT license, see the LICENSE file or <http://opensource.org/licenses/MIT>
 
 #![allow(deprecated)]
+#![cfg_attr(feature = "cargo-clippy", allow(let_and_return))]
+#![cfg_attr(feature = "cargo-clippy", allow(too_many_arguments))]
+#![cfg_attr(feature = "cargo-clippy", allow(type_complexity))]
 
 #[macro_use]
 extern crate bitflags;
