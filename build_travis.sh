ebf1a6513dffbb889a7698aab0efe6e50cdbb88a
Evgenii Pashkin <eapashkin@gmail.com>
2018-07-02 21:44:08 +0300

    examples: Add gtk 3.22.30 to travis

--- build_travis.sh
+++ build_travis.sh
@@ -3,7 +3,16 @@
 set -x
 set -e
 
-if [ "$GTK" = latest -o "$GTK" = "3.18" ]; then
+if [ "$GTK" = latest -o "$GTK" = "3.22.30" ]; then
+	BUNDLE="gtk-3.22.30-1"
+	if [ "$TRAVIS_RUST_VERSION" = "nightly" ]; then
+		FEATURES=gtk_3_22_30,futures-stable
+# Disabled as nightly not compiles now
+#		FEATURES=gtk_3_22_30,futures-nightly
+	else
+		FEATURES=gtk_3_22_30,futures-stable
+	fi
+elif [ "$GTK" = "3.18" ]; then
 	BUNDLE="gtk-3.18.1-2"
 	if [ "$TRAVIS_RUST_VERSION" = "nightly" ]; then
 		FEATURES=gtk_3_18,futures-stable
@@ -17,7 +26,7 @@ fi
 if [ -n "$BUNDLE" ]; then
 	WD="$PWD"
 	cd "$HOME"
-	curl -LO "https://github.com/gkoz/gtk-bootstrap/releases/download/$BUNDLE/deps.txz"
+	curl -LO "https://github.com/EPashkin/gtk-bootstrap/releases/download/$BUNDLE/deps.txz"
 	tar xf deps.txz
 	cd "$WD"
 	export PKG_CONFIG_PATH="$HOME/local/lib/pkgconfig"
