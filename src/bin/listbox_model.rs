3bd1bddabede6330a966bd5eb3f0e919b76b851b
Sebastian Dröge <sebastian@centricular.com>
2018-07-11 09:32:47 +0300

    examples: Add lots of comments to the code to explain what is actually happening here

--- src/bin/listbox_model.rs
+++ src/bin/listbox_model.rs
@@ -1,7 +1,14 @@
 //! # ListBox and ListModel Sample
 //!
-//! This sample demonstrates how to use gtk::Listbox in combination with
+//! This sample demonstrates how to use gtk::ListBox in combination with
 //! gio::ListStore as a model with a custom row type.
+//!
+//! It sets up a gtk::ListBox containing, per row, a label, spinbutton and
+//! an edit button. The edit button allows to edit the underlying data structure
+//! and changes are taking place immediately in the listbox by making use of GObject
+//! property bindings.
+//!
+//! In addition it is possible to add new rows and delete old ones.
 
 #[macro_use]
 extern crate glib;
@@ -53,7 +60,17 @@ fn build_ui(application: &gtk::Application) {
 
     let vbox = gtk::Box::new(gtk::Orientation::Vertical, 5);
 
+    // Create our list store and specify that the type stored in the
+    // list should be the RowData GObject we define at the bottom
     let model = gio::ListStore::new(RowData::static_type());
+
+    // And then create the UI part, the listbox and bind the list store
+    // model to it. Whenever the UI needs to show a new row, e.g. because
+    // it was notified that the model changed, it will call the callback
+    // with the corresponding item from the model and will ask for a new
+    // gtk::ListBoxRow that should be displayed.
+    //
+    // The gtk::ListBoxRow can contain any possible widgets.
     let listbox = gtk::ListBox::new();
     listbox.bind_model(&model, clone!(window => move |item| {
         let box_ = gtk::ListBoxRow::new();
@@ -61,6 +78,15 @@ fn build_ui(application: &gtk::Application) {
 
         let hbox = gtk::Box::new(gtk::Orientation::Horizontal, 5);
 
+        // Create the label and spin button that shows the two values
+        // of the item. We bind the properties for the two values to the
+        // corresponding properties of the widgets so that they are automatically
+        // updated whenever the item is changing. By specifying SYNC_CREATE the
+        // widget will automatically get the initial value of the item set.
+        //
+        // In case of the spin button the binding is bidirectional, that is any
+        // change of value in the spin button will be automatically reflected in
+        // the item.
         let label = gtk::Label::new(None);
         item.bind_property("name", &label, "label")
             .flags(glib::BindingFlags::DEFAULT | glib::BindingFlags::SYNC_CREATE)
@@ -73,6 +99,8 @@ fn build_ui(application: &gtk::Application) {
             .build();
         hbox.pack_start(&spin_button, false, false, 0);
 
+        // When the edit button is clicked, a new modal dialog is created for editing
+        // the corresponding row
         let edit_button = gtk::Button::new_with_label("Edit");
         edit_button.connect_clicked(clone!(window, item => move |_| {
             let dialog = gtk::Dialog::new_with_buttons(Some("Edit Item"), Some(&window), gtk::DialogFlags::MODAL,
@@ -82,10 +110,17 @@ fn build_ui(application: &gtk::Application) {
 
             let content_area = dialog.get_content_area();
 
+            // Similarly to the label and spin button inside the listbox, the text entry
+            // and spin button in the edit dialog are connected via property bindings to
+            // the item. Any changes will be immediately reflected inside the item and
+            // by the listbox
             let entry = gtk::Entry::new();
             item.bind_property("name", &entry, "text")
                 .flags(glib::BindingFlags::DEFAULT | glib::BindingFlags::SYNC_CREATE | glib::BindingFlags::BIDIRECTIONAL)
                 .build();
+
+            // Activating the entry (enter) will send response 0 to the dialog, which
+            // is the response code we used for the Close button. It will close the dialog
             entry.connect_activate(clone!(dialog => move |_| {
                 dialog.response(0);
             }));
@@ -103,6 +138,8 @@ fn build_ui(application: &gtk::Application) {
 
         box_.add(&hbox);
 
+        // When a row is activated (select + enter) we simply emit the clicked
+        // signal on the corresponding edit button to open the edit dialog
         box_.connect_activate(clone!(edit_button => move |_| {
             edit_button.emit_clicked();
         }));
@@ -116,6 +153,12 @@ fn build_ui(application: &gtk::Application) {
     scrolled_window.add(&listbox);
 
     let hbox = gtk::Box::new(gtk::Orientation::Horizontal, 5);
+
+    // The add button opens a new dialog which is basically the same as the edit
+    // dialog, except that we don't have a corresponding item yet at that point
+    // and only create it once the Ok button in the dialog is clicked, and only
+    // then add it to the model. Once added to the model, it will immediately
+    // appear in the listbox UI
     let add_button = gtk::Button::new_with_label("Add");
     add_button.connect_clicked(clone!(window, model => move |_| {
             let dialog = gtk::Dialog::new_with_buttons(Some("Add Item"), Some(&window), gtk::DialogFlags::MODAL,
@@ -147,6 +190,9 @@ fn build_ui(application: &gtk::Application) {
 
     hbox.add(&add_button);
 
+    // Via the delete button we delete the item from the model that
+    // is at the index of the selected row. Also deleting from the
+    // model is immediately reflected in the listbox.
     let delete_button = gtk::Button::new_with_label("Delete");
     delete_button.connect_clicked(clone!(model, listbox => move |_| {
         let selected = listbox.get_selected_row();
@@ -184,6 +230,10 @@ fn main() {
 }
 
 // Our GObject subclass for carrying a name and count for the ListBox model
+//
+// Both name and count are stored in a RefCell to allow for interior mutability
+// and are exposed via normal GObject properties. This allows us to use property
+// bindings below to bind the values with what widgets display in the UI
 mod row_data {
     use super::*;
 
@@ -194,36 +244,47 @@ mod row_data {
     use std::ptr;
     use std::mem;
 
+    // Implementation sub-module of the GObject
     mod imp {
         use super::*;
         use std::cell::RefCell;
 
+        // The actual data structure that stores our values. This is not accessible
+        // directly from the outside.
         pub struct RowData {
             name: RefCell<Option<String>>,
             count: RefCell<u32>,
         }
 
+        // GObject property definitions for our two values
         static PROPERTIES: [Property; 2] = [
             Property::String(
                 "name",
                 "Name",
                 "Name",
-                None,
+                None, // Default value
                 PropertyMutability::ReadWrite,
             ),
             Property::UInt(
                 "count",
                 "Count",
                 "Count",
-                (0, 100), 0,
+                (0, 100), 0, // Allowed range and default value
                 PropertyMutability::ReadWrite,
             ),
         ];
 
         impl RowData {
+            // glib::Type registration of the RowData type. The very first time
+            // this registers the type with GObject and afterwards only returns
+            // the type id that was registered the first time
             pub fn get_type() -> glib::Type {
                 use std::sync::{Once, ONCE_INIT};
 
+                // unsafe code here because static mut variables are inherently
+                // unsafe. Via std::sync::Once we guarantee here that the variable
+                // is only ever set once, and from that point onwards is only ever
+                // read, which makes its usage safe.
                 static ONCE: Once = ONCE_INIT;
                 static mut TYPE: glib::Type = glib::Type::Invalid;
 
@@ -237,10 +298,16 @@ mod row_data {
                 unsafe { TYPE }
             }
 
+            // Called exactly once before the first instantiation of an instance. This
+            // sets up any type-specific things, in this specific case it installs the
+            // properties so that GObject knows about their existence and they can be
+            // used on instances of our type
             fn class_init(klass: &mut ObjectClass) {
                 klass.install_properties(&PROPERTIES);
             }
 
+            // Called once at the very beginning of instantiation of each instance and
+            // creates the data structure that contains all our state
             fn init(_obj: &Object) -> Box<ObjectImpl<Object>> {
                 let imp = Self {
                     name: RefCell::new(None),
@@ -250,6 +317,12 @@ mod row_data {
             }
         }
 
+        // The ObjectImpl trait provides the setters/getters for GObject properties.
+        // Here we need to provide the values that are internally stored back to the
+        // caller, or store whatever new value the caller is providing.
+        //
+        // This maps between the GObject properties and our internal storage of the
+        // corresponding values of the properties.
         impl ObjectImpl<Object> for RowData {
             fn set_property(&self, _obj: &glib::Object, id: u32, value: &glib::Value) {
                 let prop = &PROPERTIES[id as usize];
@@ -278,6 +351,11 @@ mod row_data {
             }
         }
 
+        // Static, per-type data that is used for actually registering the type
+        // and providing the name of our type and how to initialize it to GObject
+        //
+        // It is used above in the get_type() function for passing that information
+        // to GObject
         struct RowDataStatic;
 
         impl ImplTypeStatic<Object> for RowDataStatic {
@@ -295,6 +373,8 @@ mod row_data {
         }
     }
 
+    // Public part of the RowData type. This behaves like a normal gtk-rs-style GObject
+    // binding
     glib_wrapper! {
         pub struct RowData(Object<imp::RowData>):
             [Object => InstanceStruct<Object>];
@@ -304,6 +384,8 @@ mod row_data {
         }
     }
 
+    // Constructor for new instances. This simply calls glib::Object::new() with
+    // initial values for our two properties and then returns the new instance
     impl RowData {
         pub fn new(name: &str, count: u32) -> RowData {
             use glib::object::Downcast;
