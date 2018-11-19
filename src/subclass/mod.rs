ade57ac86e7c6d48669944e61a75684d0831d564
Sebastian Dröge <sebastian@centricular.com>
2018-11-20 00:32:55 +0200

    glib: Update license/copyright headers

--- src/subclass/mod.rs
+++ src/subclass/mod.rs
@@ -1,10 +1,6 @@
-// Copyright (C) 2016-2018 Sebastian Dröge <sebastian@centricular.com>
-//
-// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
-// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
-// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
-// option. This file may not be copied, modified, or distributed
-// except according to those terms.
+// Copyright 2017-2018, The Gtk-rs Project Developers.
+// See the COPYRIGHT file at the top-level directory of this distribution.
+// Licensed under the MIT license, see the LICENSE file or <http://opensource.org/licenses/MIT>
 
 //! Module containing infrastructure for subclassing `GObject`s.
 //!
