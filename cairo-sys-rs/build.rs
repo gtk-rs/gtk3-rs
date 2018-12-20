f2a3e3506c9bb6dd368c6bdc0a406255fbf20e4b
Kornel Lesiński <kornel@geekhood.net>
2018-12-20 23:36:52 +0000

    cairo: Expose include dirs to other build scripts

--- cairo-sys-rs/build.rs
+++ cairo-sys-rs/build.rs
@@ -30,6 +30,9 @@ fn find(package_name: &str, shared_libs: &[&str]) -> Result<(), Error> {
         "1.10"
     };
 
+    if let Ok(inc_dir) = env::var("GTK_INCLUDE_DIR") {
+        println!("cargo:include={}", inc_dir);
+    }
     if let Ok(lib_dir) = env::var("GTK_LIB_DIR") {
         for lib_ in shared_libs.iter() {
             println!("cargo:rustc-link-lib=dylib={}", lib_);
@@ -50,6 +53,10 @@ fn find(package_name: &str, shared_libs: &[&str]) -> Result<(), Error> {
     }
     match config.probe(package_name) {
         Ok(library) => {
+            if let Ok(paths) = std::env::join_paths(library.include_paths) {
+                // Exposed to other build scripts as DEP_CAIRO_INCLUDE; use env::split_paths
+                println!("cargo:include={}", paths.to_string_lossy());
+            }
             if hardcode_shared_libs {
                 for lib_ in shared_libs.iter() {
                     println!("cargo:rustc-link-lib=dylib={}", lib_);
