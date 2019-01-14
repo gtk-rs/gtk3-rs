c7872af052242f8a586ecc1f1c541e5e3d23de9a
Sebastian Dröge <sebastian@centricular.com>
2019-01-14 14:49:58 +0200

    atk: Update manual code for glib API changes

--- src/editable_text.rs
+++ src/editable_text.rs
@@ -7,7 +7,7 @@ use glib::translate::*;
 use ffi;
 use EditableText;
 
-pub trait EditableTextExtManual {
+pub trait EditableTextExtManual: 'static {
     fn insert_text(&self, string: &str, position: i32) -> i32;
 }
 
@@ -15,7 +15,7 @@ impl<O: IsA<EditableText>> EditableTextExtManual for O {
     fn insert_text(&self, string: &str, mut position: i32) -> i32 {
         let length = string.len() as i32;
         unsafe {
-            ffi::atk_editable_text_insert_text(self.to_glib_none().0, string.to_glib_none().0, length, &mut position);
+            ffi::atk_editable_text_insert_text(self.as_ref().to_glib_none().0, string.to_glib_none().0, length, &mut position);
         }
         position
     }
