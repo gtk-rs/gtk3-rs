f3632ecc306d00c12521bdec36f4ae6a9828b6c3
Sebastian Dröge <sebastian@centricular.com>
2019-02-06 18:41:25 +0200

    examples: Update minimum versions and add 3.24 to the travis CI

--- build_travis.sh
+++ build_travis.sh
@@ -3,19 +3,26 @@
 set -x
 set -e
 
-if [ "$GTK" = latest -o "$GTK" = "3.22.30" ]; then
+if [ "$GTK" = latest -o "$GTK" = "3.24" ]; then
+	BUNDLE="gtk-3.24.0-1"
+	if [ "$TRAVIS_RUST_VERSION" = "nightly" ]; then
+		FEATURES=gtk_3_24,futures-stable,gio/v2_44,subclassing
+	else
+		FEATURES=gtk_3_24,futures-stable,gio/v2_44,subclassing
+	fi
+elif [ "$GTK" = "3.22.30" ]; then
 	BUNDLE="gtk-3.22.30-1"
 	if [ "$TRAVIS_RUST_VERSION" = "nightly" ]; then
-		FEATURES=gtk_3_22_30,futures-stable,gio_2_40,subclassing
+		FEATURES=gtk_3_22_30,futures-stable,gio/v2_44,subclassing
 	else
-		FEATURES=gtk_3_22_30,futures-stable,gio_2_40,subclassing
+		FEATURES=gtk_3_22_30,futures-stable,gio/v2_44,subclassing
 	fi
 elif [ "$GTK" = "3.18" ]; then
 	BUNDLE="gtk-3.18.1-2"
 	if [ "$TRAVIS_RUST_VERSION" = "nightly" ]; then
-		FEATURES=gtk_3_18,futures-stable,gio_2_40,subclassing
+		FEATURES=gtk_3_18,futures-stable,gio/v2_44,subclassing
 	else
-		FEATURES=gtk_3_18,futures-stable,gio_2_40,subclassing
+		FEATURES=gtk_3_18,futures-stable,gio/v2_44,subclassing
 	fi
 fi
 
