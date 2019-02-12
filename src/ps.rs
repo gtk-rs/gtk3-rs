672cf26e8815567c3b7aa8526d40ba84b995bc40
Guillaume Gomez <guillaume1.gomez@gmail.com>
2019-02-12 17:53:02 +0100

    cairo: Remove doc comments

--- src/ps.rs
+++ src/ps.rs
@@ -36,7 +36,6 @@ pub fn level_to_string(level: PsLevel) -> Option<&'static str> {
     }
 }
 
-/// A PostScript surface that writes to a file
 pub struct File {
     inner: Surface,
 }
@@ -157,14 +156,6 @@ impl AsRef<File> for File {
 }
 
 
-/// A PostScript surface that writes to a generic `io::Write` type (owning variant)
-///
-/// The `Writer` takes ownership of the write object.
-/// Once you're done using the surface, you can obtain the write object
-/// back using the `finish()` method.
-///
-/// If you would like the surface to reference the write object
-/// instead, use `RefWriter`.
 pub struct Writer<W: io::Write> {
     writer: support::Writer<File, W>,
 }
@@ -223,13 +214,6 @@ impl<'a, W: io::Write> ToGlibPtr<'a, *mut ffi::cairo_surface_t> for Writer<W> {
 }
 
 
-/// A PostScript surface that writes to a generic `io::Write` type (referencing variant)
-///
-/// The `RefWriter` references the write object,
-/// which is why a lifetime parameter is required.
-///
-/// If you would like the surface to own the write object
-/// instead, use `Writer`.
 pub struct RefWriter<'w, W: io::Write + 'w> {
     writer: support::RefWriter<'w, File, W>,
 }
