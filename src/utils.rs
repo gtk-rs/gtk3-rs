gtk-rs/glib

commit 27843d0bd48b872f81da854e5d11d391ff856595

Fix manual bindings for GString support

src/utils.rs
@@ -10,6 +10,7 @@ use std::path::{Path, PathBuf};
 use error::BoolError;
 use Error;
 use std::ptr;
+use gstring::GString;
 
 /// Same as [`get_prgname()`].
 ///
@@ -112,7 +113,7 @@ pub fn get_current_dir() -> Option<PathBuf> {
     }
 }
 
-pub fn filename_to_uri<'a, P: AsRef<Path>, Q: Into<Option<&'a str>>>(filename: P, hostname: Q) -> Result<String, Error> {
+pub fn filename_to_uri<'a, P: AsRef<Path>, Q: Into<Option<&'a str>>>(filename: P, hostname: Q) -> Result<GString, Error> {
     #[cfg(windows)]
     use ffi::g_filename_to_uri_utf8 as g_filename_to_uri;
     #[cfg(not(windows))]
@@ -127,7 +128,7 @@ pub fn filename_to_uri<'a, P: AsRef<Path>, Q: Into<Option<&'a str>>>(filename: P
     }
 }
 
-pub fn filename_from_uri(uri: &str) -> Result<(std::path::PathBuf, Option<String>), Error> {
+pub fn filename_from_uri(uri: &str) -> Result<(std::path::PathBuf, Option<GString>), Error> {
     #[cfg(windows)]
     use ffi::g_filename_from_uri_utf8 as g_filename_from_uri;
     #[cfg(not(windows))]
@@ -137,7 +138,7 @@ pub fn filename_from_uri(uri: &str) -> Result<(std::path::PathBuf, Option<String
         let mut hostname = ptr::null_mut();
         let mut error = ptr::null_mut();
         let ret = g_filename_from_uri(uri.to_glib_none().0, &mut hostname, &mut error);
-        if error.is_null() { Ok((from_glib_full(ret), from_glib_full(hostname))) } else { Err(from_glib_full(error)) }
+        if error.is_null() { Ok((from_glib_full(ret), Some(from_glib_full(hostname)))) } else { Err(from_glib_full(error)) }
     }
 }
 

src/key_file.rs
@@ -11,6 +11,7 @@ use std::ptr;
 use std::path;
 use error::Error;
 use auto::KeyFileFlags;
+use gstring::GString;
 
 use KeyFile;
 
@@ -37,8 +38,8 @@ impl KeyFile {
                                                         &mut full_path,
                                                         flags.to_glib(), &mut error);
             if error.is_null() {
-                let path: String = from_glib_full(full_path);
-                Ok(path::PathBuf::from(path))
+                let path: GString = from_glib_full(full_path);
+                Ok(path::PathBuf::from(&path))
             } else {
                 Err(from_glib_full(error))
             }
@@ -57,15 +58,15 @@ impl KeyFile {
                                                    &mut full_path,
                                                    flags.to_glib(), &mut error);
             if error.is_null() {
-                let path: String = from_glib_full(full_path);
-                Ok(path::PathBuf::from(path))
+                let path: GString = from_glib_full(full_path);
+                Ok(path::PathBuf::from(&path))
             } else {
                 Err(from_glib_full(error))
             }
         }
     }
 
-    pub fn to_data(&self) -> String {
+    pub fn to_data(&self) -> GString {
         unsafe {
             let ret = ffi::g_key_file_to_data(self.to_glib_none().0, ptr::null_mut(), ptr::null_mut());
             from_glib_full(ret)

src/variant.rs
@@ -52,6 +52,7 @@ use std::fmt;
 use std::hash::{Hash, Hasher};
 use std::slice;
 use std::str;
+use gstring::GString;
 
 glib_wrapper! {
     /// A generic immutable value capable of carrying various types.
@@ -144,7 +145,7 @@ impl fmt::Debug for Variant {
 
 impl fmt::Display for Variant {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        let serialized: String = unsafe {
+        let serialized: GString = unsafe {
             from_glib_full(glib_ffi::g_variant_print(self.to_glib_none().0, false.to_glib()))
         };
         f.write_str(&serialized)

gtk-rs/gtk3-rs

commit 09210ed069f4b5ed20f3c83b74f8873b070dc009

glib: utils: Fix crash in filename_from_uri()
Introduced by commit 27843d0bd48b872f81da854e5d11d391ff856595.

src/utils.rs
@@ -142,7 +142,7 @@ pub fn filename_from_uri(uri: &str) -> Result<(std::path::PathBuf, Option<GStrin
         let mut hostname = ptr::null_mut();
         let mut error = ptr::null_mut();
         let ret = g_filename_from_uri(uri.to_glib_none().0, &mut hostname, &mut error);
-        if error.is_null() { Ok((from_glib_full(ret), Some(from_glib_full(hostname)))) } else { Err(from_glib_full(error)) }
+        if error.is_null() { Ok((from_glib_full(ret), from_glib_full(hostname))) } else { Err(from_glib_full(error)) }
     }
 }
 
@@ -228,5 +228,27 @@ mod tests {
     fn setenv() {
         check_setenv("Test");
         check_setenv("Тест"); // "Test" in Russian
+    }
+
+    #[test]
+    fn test_filename_from_uri() {
+        use gstring::GString;
+        use std::path::PathBuf;
+        let uri: GString = "file:///foo/bar.txt".into();
+        if let Ok((filename, hostname)) = ::filename_from_uri(&uri) {
+            assert_eq!(filename, PathBuf::from(r"/foo/bar.txt"));
+            assert_eq!(hostname, None);
+        } else {
+            unreachable!();
+        }
+
+        let uri: GString = "file://host/foo/bar.txt".into();
+        if let Ok((filename, hostname)) = ::filename_from_uri(&uri) {
+            assert_eq!(filename, PathBuf::from(r"/foo/bar.txt"));
+            assert_eq!(hostname, Some(GString::from("host")));
+        } else {
+            unreachable!();
+        }
+
     }
 }