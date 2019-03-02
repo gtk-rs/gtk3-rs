6b32d8fd3ed0abec6db81ab29e9a2ebd14dbdc26
Xiang Fan <sfanxiang@gmail.com>
2019-03-02 21:16:45 +0800

    graphene: Update build.rs

--- build.rs
+++ build.rs
@@ -14,7 +14,7 @@ fn manage_docs () {
     ];
     lgpl_docs::purge(PATH, IGNORES);
     if cfg!(feature = "embed-lgpl-docs") {
-        lgpl_docs::embed(lgpl_docs::Library::Gdk, PATH, IGNORES);
+        lgpl_docs::embed(lgpl_docs::Library::Graphene, PATH, IGNORES);
     }
 }
 
