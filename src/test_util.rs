866f05f1b87e41405c317e908d95ec1ada1ca01b
Guillaume Gomez <guillaume1.gomez@gmail.com>
2019-03-09 17:12:15 +0100

    gio: Fix failing test compilation

--- src/test_util.rs
+++ src/test_util.rs
@@ -9,7 +9,7 @@ use glib::*;
 #[allow(dead_code)]
 pub fn run_async<T: Send + 'static, Q: FnOnce(Sender<T>, MainLoop) + Send + 'static>(start: Q) -> T {
     let c = MainContext::new();
-    let l = MainLoop::new(&c, false);
+    let l = MainLoop::new(Some(&c), false);
     let l_clone = l.clone();
 
     let (tx, rx) = channel();
