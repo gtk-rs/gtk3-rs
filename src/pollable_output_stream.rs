c805de77aa16c5447bbe06bc630d1d5f9a949144
Sebastian Dröge <sebastian@centricular.com>
2019-11-20 17:46:31 +0200

    gio: Get rid of remaining, unneeded fragile usage
    And clean up pollable in/output stream and socket GSource code.

--- src/pollable_output_stream.rs
+++ src/pollable_output_stream.rs
@@ -3,7 +3,6 @@
 // Licensed under the MIT license, see the LICENSE file or <http://opensource.org/licenses/MIT>
 
 use error::to_std_io_result;
-use fragile::Fragile;
 use futures_channel::oneshot;
 use futures_core::task::{Context, Poll};
 use futures_core::Future;
@@ -72,21 +71,19 @@ impl<O: IsA<PollableOutputStream>> PollableOutputStreamExtManual for O {
         F: FnMut(&Self) -> glib::Continue + 'static,
         C: IsA<Cancellable>,
     {
-        #[allow(clippy::transmute_ptr_to_ref)]
-        unsafe extern "C" fn trampoline<O: IsA<PollableOutputStream>>(
+        unsafe extern "C" fn trampoline<
+            O: IsA<PollableOutputStream>,
+            F: FnMut(&O) -> glib::Continue + 'static,
+        >(
             stream: *mut gio_sys::GPollableOutputStream,
             func: glib_sys::gpointer,
         ) -> glib_sys::gboolean {
-            let func: &Fragile<RefCell<Box<dyn FnMut(&O) -> glib::Continue + 'static>>> =
-                transmute(func);
-            let func = func.get();
+            let func: &RefCell<F> = &*(func as *const RefCell<F>);
             let mut func = func.borrow_mut();
             (&mut *func)(&PollableOutputStream::from_glib_borrow(stream).unsafe_cast()).to_glib()
         }
-        unsafe extern "C" fn destroy_closure<O>(ptr: glib_sys::gpointer) {
-            Box::<Fragile<RefCell<Box<dyn FnMut(&O) -> glib::Continue + 'static>>>>::from_raw(
-                ptr as *mut _,
-            );
+        unsafe extern "C" fn destroy_closure<O, F>(ptr: glib_sys::gpointer) {
+            Box::<RefCell<F>>::from_raw(ptr as *mut _);
         }
         let cancellable = cancellable.map(|c| c.as_ref());
         let gcancellable = cancellable.to_glib_none();
@@ -96,12 +93,12 @@ impl<O: IsA<PollableOutputStream>> PollableOutputStreamExtManual for O {
                 gcancellable.0,
             );
 
-            let trampoline = trampoline::<Self> as glib_sys::gpointer;
+            let trampoline = trampoline::<Self, F> as glib_sys::gpointer;
             glib_sys::g_source_set_callback(
                 source,
                 Some(transmute(trampoline)),
-                into_raw(func),
-                Some(destroy_closure::<Self>),
+                Box::into_raw(Box::new(RefCell::new(func))) as glib_sys::gpointer,
+                Some(destroy_closure::<Self, F>),
             );
             glib_sys::g_source_set_priority(source, priority.to_glib());
 
@@ -120,14 +117,13 @@ impl<O: IsA<PollableOutputStream>> PollableOutputStreamExtManual for O {
     ) -> Pin<Box<dyn std::future::Future<Output = ()> + 'static>> {
         let cancellable: Option<Cancellable> = cancellable.map(|c| c.as_ref()).cloned();
 
-        let obj = Fragile::new(self.clone());
+        let obj = self.clone();
         Box::pin(glib::SourceFuture::new(move |send| {
             let mut send = Some(send);
-            obj.get()
-                .create_source(cancellable.as_ref(), None, priority, move |_| {
-                    let _ = send.take().unwrap().send(());
-                    glib::Continue(false)
-                })
+            obj.create_source(cancellable.as_ref(), None, priority, move |_| {
+                let _ = send.take().unwrap().send(());
+                glib::Continue(false)
+            })
         }))
     }
 
@@ -138,17 +134,16 @@ impl<O: IsA<PollableOutputStream>> PollableOutputStreamExtManual for O {
     ) -> Pin<Box<dyn Stream<Item = ()> + 'static>> {
         let cancellable: Option<Cancellable> = cancellable.map(|c| c.as_ref()).cloned();
 
-        let obj = Fragile::new(self.clone());
+        let obj = self.clone();
         Box::pin(glib::SourceStream::new(move |send| {
             let send = Some(send);
-            obj.get()
-                .create_source(cancellable.as_ref(), None, priority, move |_| {
-                    if send.as_ref().unwrap().unbounded_send(()).is_err() {
-                        glib::Continue(false)
-                    } else {
-                        glib::Continue(true)
-                    }
-                })
+            obj.create_source(cancellable.as_ref(), None, priority, move |_| {
+                if send.as_ref().unwrap().unbounded_send(()).is_err() {
+                    glib::Continue(false)
+                } else {
+                    glib::Continue(true)
+                }
+            })
         }))
     }
 }
@@ -263,10 +258,4 @@ impl<T: IsA<PollableOutputStream>> AsyncWrite for OutputStreamAsyncWrite<T> {
             Poll::Pending => Poll::Pending,
         }
     }
-}
-
-fn into_raw<O, F: FnMut(&O) -> glib::Continue + 'static>(func: F) -> glib_sys::gpointer {
-    let func: Box<Fragile<RefCell<Box<dyn FnMut(&O) -> glib::Continue + 'static>>>> =
-        Box::new(Fragile::new(RefCell::new(Box::new(func))));
-    Box::into_raw(func) as glib_sys::gpointer
 }
