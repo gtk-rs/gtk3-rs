5144a96ba70da49afef4037523e26044f8744f92
Sebastian Dröge <sebastian@centricular.com>
2020-01-02 10:42:29 +0200

    pango: Regenerate with latest gir

--- src/auto/constants.rs
+++ src/auto/constants.rs
@@ -6,26 +6,23 @@ use pango_sys;
 use std::ffi::CStr;
 
 #[cfg_attr(feature = "v1_38", deprecated)]
-lazy_static! {
-    pub static ref ENGINE_TYPE_LANG: &'static str = unsafe {
+pub static ENGINE_TYPE_LANG: once_cell::sync::Lazy<&'static str> =
+    once_cell::sync::Lazy::new(|| unsafe {
         CStr::from_ptr(pango_sys::PANGO_ENGINE_TYPE_LANG)
             .to_str()
             .unwrap()
-    };
-}
+    });
 #[cfg_attr(feature = "v1_38", deprecated)]
-lazy_static! {
-    pub static ref ENGINE_TYPE_SHAPE: &'static str = unsafe {
+pub static ENGINE_TYPE_SHAPE: once_cell::sync::Lazy<&'static str> =
+    once_cell::sync::Lazy::new(|| unsafe {
         CStr::from_ptr(pango_sys::PANGO_ENGINE_TYPE_SHAPE)
             .to_str()
             .unwrap()
-    };
-}
+    });
 #[cfg_attr(feature = "v1_38", deprecated)]
-lazy_static! {
-    pub static ref RENDER_TYPE_NONE: &'static str = unsafe {
+pub static RENDER_TYPE_NONE: once_cell::sync::Lazy<&'static str> =
+    once_cell::sync::Lazy::new(|| unsafe {
         CStr::from_ptr(pango_sys::PANGO_RENDER_TYPE_NONE)
             .to_str()
             .unwrap()
-    };
-}
+    });
