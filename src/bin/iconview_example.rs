432a4bc366fc6658cfb503ea28455b1e003e5501
artonio <asukhovatkin@gmail.com>
2020-01-03 13:56:56 -0500

    examples: run cargo fmt

--- src/bin/iconview_example.rs
+++ src/bin/iconview_example.rs
@@ -2,14 +2,14 @@
 //!
 //! This sample demonstrates how to create a toplevel `window`, set its title, size and
 //! position, how to add a `IconView` to this `window` and how to set `model` of the `IconView`
-//! 
+//!
 //! A Gtk.IconView is a widget that displays a collection of icons in a grid view.
 //! It supports features such as drag and drop, multiple selections and item reordering.
 //! Similarly to Gtk.TreeView, Gtk.IconView uses a Gtk.ListStore for its model.
-//! 
+//!
 //! Instead of using cell renderers, Gtk.IconView requires that one of the columns in its
 //! Gtk.ListStore contains GdkPixbuf.Pixbuf objects.
-//! 
+//!
 //! The example is using icons from the current icon theme. To view all icons and their names please
 //! install gtk3-icon-browser: https://developer.gnome.org/gtk3/stable/gtk3-icon-browser.html
 
