d28377cfc521e5bfc5aa960a04b7b3a7c4a925d2
artonio <asukhovatkin@gmail.com>
2020-01-03 12:09:16 -0500

    examples: cargo fmt

--- src/bin/iconview_example.rs
+++ src/bin/iconview_example.rs
@@ -2,10 +2,10 @@
 //!
 //! This sample demonstrates how to create a toplevel `window`, set its title, size and
 //! position, how to add a `IconView` to this `window` and how to set `model` of the `IconView`
-//! A Gtk.IconView is a widget that displays a collection of icons in a grid view. 
+//! A Gtk.IconView is a widget that displays a collection of icons in a grid view.
 //! It supports features such as drag and drop, multiple selections and item reordering.
-//! Similarly to Gtk.TreeView, Gtk.IconView uses a Gtk.ListStore for its model. 
-//! Instead of using cell renderers, Gtk.IconView requires that one of the columns in its 
+//! Similarly to Gtk.TreeView, Gtk.IconView uses a Gtk.ListStore for its model.
+//! Instead of using cell renderers, Gtk.IconView requires that one of the columns in its
 //! Gtk.ListStore contains GdkPixbuf.Pixbuf objects.
 //! The example is using icons from the current icon theme. To view all icons and their names please install
 //! gtk3-icon-browser: https://developer.gnome.org/gtk3/stable/gtk3-icon-browser.html
@@ -22,7 +22,7 @@ use std::process;
 // Concinience Enum for IconView column types
 enum IconViewColumnType {
     TextColumn = 0,
-    PixbufColumn = 1
+    PixbufColumn = 1,
 }
 
 fn create_list_store_model() -> gtk::ListStore {
@@ -34,26 +34,32 @@ fn create_list_store_model() -> gtk::ListStore {
     let col_types: [glib::Type; 2] = [glib::Type::String, gdk_pixbuf::Pixbuf::static_type()];
     let icon_view_model = gtk::ListStore::new(&col_types);
 
-    // IconTheme provides a facility for looking up icons by name and size. 
+    // IconTheme provides a facility for looking up icons by name and size.
     // Get default icon theme
     let icon_theme: Option<gtk::IconTheme> = gtk::IconTheme::get_default();
     if let Some(it) = icon_theme {
         for x in &icons {
-            // Looks up an icon in an icon theme, scales it to the given size and renders it into a pixbuf. 
+            // Looks up an icon in an icon theme, scales it to the given size and renders it into a pixbuf.
             let result = it.load_icon(x, 64, gtk::IconLookupFlags::empty());
             match result {
                 Ok(r) => {
                     // Notice how we specified the first column to be Text and second to be Pixbuf just like in col_types var
                     // The values also follow the same order, &[&String::from("Label"), &r]. First item is text, second is pixbuf
-                    icon_view_model.insert_with_values(None, &[IconViewColumnType::TextColumn as u32, IconViewColumnType::PixbufColumn as u32], &[&String::from("Label"), &r]);
-                },
+                    icon_view_model.insert_with_values(
+                        None,
+                        &[
+                            IconViewColumnType::TextColumn as u32,
+                            IconViewColumnType::PixbufColumn as u32,
+                        ],
+                        &[&String::from("Label"), &r],
+                    );
+                }
                 Err(err) => {
                     println!("Error: {}", err);
                     process::exit(1);
                 }
             }
         }
-
     }
 
     return icon_view_model;
@@ -61,8 +67,6 @@ fn create_list_store_model() -> gtk::ListStore {
 
 fn build_ui(application: &gtk::Application) {
     let window = gtk::ApplicationWindow::new(application);
-
-    
 
     window.set_title("IconView Example");
     window.set_border_width(10);
@@ -84,15 +88,17 @@ fn build_ui(application: &gtk::Application) {
     // And finally set text column and pixbuf column using enum
     icon_view.set_text_column(IconViewColumnType::TextColumn as i32);
     icon_view.set_pixbuf_column(IconViewColumnType::PixbufColumn as i32);
-    
+
     window.add(&icon_view);
     window.show_all();
 }
 
 fn main() {
-    let application =
-        gtk::Application::new(Some("com.github.gtk-rs.examples.iconview_example"), Default::default())
-            .expect("Initialization failed...");
+    let application = gtk::Application::new(
+        Some("com.github.gtk-rs.examples.iconview_example"),
+        Default::default(),
+    )
+    .expect("Initialization failed...");
 
     application.connect_activate(|app| {
         build_ui(app);
