478c7388954b98f0f7d8f8f2a2c2a4229928d67a
Evgenii Pashkin <eapashkin@gmail.com>
2020-01-26 11:40:10 +0300

    examples: Do check only on cross-compilation

--- build_travis.sh
+++ build_travis.sh
@@ -35,4 +35,8 @@ if [ -n "$BUNDLE" ] && [ "$TRAVIS_OS_NAME" != "osx" ]; then
 	export PKG_CONFIG_PATH="$HOME/local/lib/pkgconfig"
 fi
 
-PKG_CONFIG_ALLOW_CROSS=1 cargo check $OTHER_TARGET --features "$FEATURES" --jobs 1 "$@"
+if [ -n "$OTHER_TARGET" ]; then
+  PKG_CONFIG_ALLOW_CROSS=1 cargo check $OTHER_TARGET --features "$FEATURES" --jobs 1 "$@"
+else
+  cargo build --features "$FEATURES" --jobs 1 "$@"
+fi
