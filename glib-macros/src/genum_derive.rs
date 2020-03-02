4e9e30826067f33b9fcadef815fa1d7acfb92c5d
Guillaume Desmottes <guillaume.desmottes@collabora.com>
2020-03-02 14:35:55 +0530

    glib: glib-macros: genum_derive: add missing copyright header

--- glib-macros/src/genum_derive.rs
+++ glib-macros/src/genum_derive.rs
@@ -1,3 +1,7 @@
+// Copyright 2020, The Gtk-rs Project Developers.
+// See the COPYRIGHT file at the top-level directory of this distribution.
+// Licensed under the MIT license, see the LICENSE file or <http://opensource.org/licenses/MIT>
+
 use anyhow::{bail, Result};
 use heck::{CamelCase, KebabCase, SnakeCase};
 use itertools::Itertools;
