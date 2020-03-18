2f85666b8d842d6ac859850d04b2efbafc488b7d
Guillaume Gomez <guillaume1.gomez@gmail.com>
2020-03-18 16:54:44 +0100

    glib: Fix glib import context issue

--- glib-macros/src/gboxed_derive.rs
+++ glib-macros/src/gboxed_derive.rs
@@ -2,8 +2,9 @@
 // See the COPYRIGHT file at the top-level directory of this distribution.
 // Licensed under the MIT license, see the LICENSE file or <http://opensource.org/licenses/MIT>
 
-use proc_macro2::{Ident, TokenStream};
+use proc_macro2::{Ident, Span, TokenStream};
 use proc_macro_error::abort_call_site;
+use proc_macro_crate::crate_name;
 use quote::quote;
 
 use crate::utils::{find_attribute_meta, find_nested_meta, parse_type_name};
@@ -40,12 +41,12 @@ fn gen_ptr_to_option(name: &Ident, nullable: bool) -> TokenStream {
     }
 }
 
-fn gen_impl_from_value(name: &Ident) -> TokenStream {
+fn gen_impl_from_value(name: &Ident, crate_ident: &Ident) -> TokenStream {
     quote! {
-        impl<'a> ::glib::value::FromValue<'a> for &'a #name {
-            unsafe fn from_value(value: &'a ::glib::value::Value) -> Self {
-                let ptr = ::glib::gobject_sys::g_value_get_boxed(
-                    ::glib::translate::ToGlibPtr::to_glib_none(value).0,
+        impl<'a> #crate_ident::value::FromValue<'a> for &'a #name {
+            unsafe fn from_value(value: &'a #crate_ident::value::Value) -> Self {
+                let ptr = #crate_ident::gobject_sys::g_value_get_boxed(
+                    #crate_ident::translate::ToGlibPtr::to_glib_none(value).0,
                 );
                 assert!(!ptr.is_null());
                 &*(ptr as *mut #name)
@@ -65,6 +66,16 @@ pub fn impl_gboxed(input: &syn::DeriveInput) -> TokenStream {
         ),
     };
 
+    let crate_name = match crate_name("glib") {
+        Ok(x) => x,
+	Err(_) => {
+            // In case we use it directly from glib itself (it cannot find glib as a dependency
+	    // in this case)
+            "glib".to_owned()
+	}
+    };
+    let crate_ident = Ident::new(&crate_name, Span::call_site());
+
     let meta = find_attribute_meta(&input.attrs, "gboxed")
         .unwrap()
         .unwrap();
@@ -73,7 +84,7 @@ pub fn impl_gboxed(input: &syn::DeriveInput) -> TokenStream {
     let option_to_ptr = gen_option_to_ptr(nullable);
     let ptr_to_option = gen_ptr_to_option(name, nullable);
     let impl_from_value = if !nullable {
-        gen_impl_from_value(name)
+        gen_impl_from_value(name, &crate_ident)
     } else {
         quote! {}
     };
@@ -82,12 +93,12 @@ pub fn impl_gboxed(input: &syn::DeriveInput) -> TokenStream {
         impl BoxedType for #name {
             const NAME: &'static str = #gtype_name;
 
-            fn get_type() -> ::glib::Type {
-                static mut TYPE_: ::glib::Type = ::glib::Type::Invalid;
+            fn get_type() -> #crate_ident::Type {
+                static mut TYPE_: #crate_ident::Type = #crate_ident::Type::Invalid;
                 static ONCE: ::std::sync::Once = ::std::sync::Once::new();
 
                 ONCE.call_once(|| {
-                    let type_ = ::glib::subclass::register_boxed_type::<Self>();
+                    let type_ = #crate_ident::subclass::register_boxed_type::<Self>();
                     unsafe {
                         TYPE_ = type_;
                     }
@@ -97,36 +108,36 @@ pub fn impl_gboxed(input: &syn::DeriveInput) -> TokenStream {
             }
         }
 
-        impl ::glib::StaticType for #name {
-            fn static_type() -> ::glib::Type {
-                <#name as ::glib::subclass::boxed::BoxedType>::get_type()
+        impl #crate_ident::StaticType for #name {
+            fn static_type() -> #crate_ident::Type {
+                <#name as #crate_ident::subclass::boxed::BoxedType>::get_type()
             }
         }
 
-        impl ::glib::value::SetValue for #name {
-            unsafe fn set_value(value: &mut ::glib::value::Value, this: &Self) {
+        impl #crate_ident::value::SetValue for #name {
+            unsafe fn set_value(value: &mut #crate_ident::value::Value, this: &Self) {
                 let ptr: *mut #name = Box::into_raw(Box::new(this.clone()));
-                ::glib::gobject_sys::g_value_take_boxed(
-                    ::glib::translate::ToGlibPtrMut::to_glib_none_mut(value).0,
+                #crate_ident::gobject_sys::g_value_take_boxed(
+                    #crate_ident::translate::ToGlibPtrMut::to_glib_none_mut(value).0,
                     ptr as *mut _,
                 );
             }
         }
 
-        impl ::glib::value::SetValueOptional for #name {
-            unsafe fn set_value_optional(value: &mut ::glib::value::Value, this: Option<&Self>) {
+        impl #crate_ident::value::SetValueOptional for #name {
+            unsafe fn set_value_optional(value: &mut #crate_ident::value::Value, this: Option<&Self>) {
                 let ptr: *mut #name = #option_to_ptr;
-                ::glib::gobject_sys::g_value_take_boxed(
-                    ::glib::translate::ToGlibPtrMut::to_glib_none_mut(value).0,
+                #crate_ident::gobject_sys::g_value_take_boxed(
+                    #crate_ident::translate::ToGlibPtrMut::to_glib_none_mut(value).0,
                     ptr as *mut _,
                 );
             }
         }
 
-        impl<'a> ::glib::value::FromValueOptional<'a> for &'a #name {
-            unsafe fn from_value_optional(value: &'a ::glib::value::Value) -> Option<Self> {
-                let ptr = ::glib::gobject_sys::g_value_get_boxed(
-                    ::glib::translate::ToGlibPtr::to_glib_none(value).0,
+        impl<'a> #crate_ident::value::FromValueOptional<'a> for &'a #name {
+            unsafe fn from_value_optional(value: &'a #crate_ident::value::Value) -> Option<Self> {
+                let ptr = #crate_ident::gobject_sys::g_value_get_boxed(
+                    #crate_ident::translate::ToGlibPtr::to_glib_none(value).0,
                 );
                 #ptr_to_option
             }
