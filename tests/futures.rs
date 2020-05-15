a4040c22fa1b4aa0249046da2b364d7c6e9afd3d
Guillaume Desmottes <guillaume.desmottes@collabora.com>
2020-05-15 15:35:33 +0200

    gio: add tests/futures.rs build with edition 2018
    Will be used to test futurs API relying on the 'async' keyword which is
    only available in Rust 2018.

---
+++ tests/futures.rs
@@ -0,0 +1,7 @@
+// Copyright 2020, The Gtk-rs Project Developers.
+// See the COPYRIGHT file at the top-level directory of this distribution.
+// Licensed under the MIT license, see the LICENSE file or <http://opensource.org/licenses/MIT>
+
+// This target is build with edition 2018 for testing futures API.
+// TODO: merge to the test module of the corresponding source files once the crate
+// has been ported to 2018.
