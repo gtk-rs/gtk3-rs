474c67df09a6a78a31a58a80cce473719bb9a259
sdroege <sdroege@users.noreply.github.com>
2020-12-09 13:25:53 +0000

    Automated deployment: Wed Dec  9 13:25:53 UTC 2020 3d86c19329de7015f46150063db90798b67e1967

--- implementors/glib/translate/trait.FromGlibContainerAsVec.js
+++ implementors/glib/translate/trait.FromGlibContainerAsVec.js
@@ -2,5 +2,5 @@
 implementors["gdk"] = [{"text":"impl FromGlibContainerAsVec&lt;*mut _GdkAtom, *mut *mut _GdkAtom&gt; for Atom","synthetic":false,"types":[]},{"text":"impl FromGlibContainerAsVec&lt;GdkTimeCoord, *mut GdkTimeCoord&gt; for TimeCoord","synthetic":false,"types":[]}];
 implementors["glib"] = [];
 implementors["gtk"] = [{"text":"impl FromGlibContainerAsVec&lt;GtkPageRange, *mut GtkPageRange&gt; for PageRange","synthetic":false,"types":[]}];
-implementors["pango"] = [{"text":"impl FromGlibContainerAsVec&lt;*mut PangoGlyphInfo, *mut PangoGlyphInfo&gt; for GlyphInfo","synthetic":false,"types":[]}];
+implementors["pango"] = [{"text":"impl FromGlibContainerAsVec&lt;*mut PangoGlyphInfo, *mut PangoGlyphInfo&gt; for GlyphInfo","synthetic":false,"types":[]},{"text":"impl FromGlibContainerAsVec&lt;*mut PangoGlyphInfo, *const PangoGlyphInfo&gt; for GlyphInfo","synthetic":false,"types":[]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
