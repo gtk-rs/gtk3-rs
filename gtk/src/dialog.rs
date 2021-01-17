ad20f813857cdafa37ba3ad908ffd22e0de95345
Sophie Herold <sophie@hemio.de>
2021-01-17 18:04:07 +0100

    Add Dialog::run_future()
    Backport of gtk-rs/gtk4-rs#149

--- gtk/src/dialog.rs
+++ gtk/src/dialog.rs
@@ -5,10 +5,15 @@ use crate::Dialog;
 use crate::DialogFlags;
 use crate::ResponseType;
 use crate::Widget;
+use crate::WidgetExt;
 use crate::Window;
 use glib::object::Cast;
 use glib::translate::*;
 use glib::IsA;
+use glib::ObjectExt;
+use std::cell::Cell;
+use std::future::Future;
+use std::pin::Pin;
 use std::ptr;
 
 impl Dialog {
@@ -38,13 +43,58 @@ impl Dialog {
 pub trait DialogExtManual: 'static {
     #[doc(alias = "gtk_dialog_add_buttons")]
     fn add_buttons(&self, buttons: &[(&str, ResponseType)]);
+
+    // rustdoc-stripper-ignore-next
+    /// Shows the dialog and returns a `Future` that resolves to the
+    /// `ResponseType` on response.
+    ///
+    /// ```no_run
+    /// use gtk::prelude::*;
+    ///
+    /// # async fn run() {
+    /// let dialog = gtk::MessageDialogBuilder::new()
+    ///    .buttons(gtk::ButtonsType::OkCancel)
+    ///    .text("What is your answer?")
+    ///    .build();
+    ///
+    /// let answer = dialog.run_future().await;
+    /// dialog.close();
+    /// println!("Answer: {:?}", answer);
+    /// # }
+    /// ```
+    fn run_future<'a>(&'a self) -> Pin<Box<dyn Future<Output = ResponseType> + 'a>>;
 }
 
-impl<O: IsA<Dialog>> DialogExtManual for O {
+impl<O: IsA<Dialog> + IsA<Widget>> DialogExtManual for O {
     fn add_buttons(&self, buttons: &[(&str, ResponseType)]) {
         for &(text, id) in buttons {
             //FIXME: self.add_button don't work on 1.8
             O::add_button(self, text, id);
         }
+    }
+
+    fn run_future<'a>(&'a self) -> Pin<Box<dyn Future<Output = ResponseType> + 'a>> {
+        Box::pin(async move {
+            let (sender, receiver) = futures_channel::oneshot::channel();
+
+            let sender = Cell::new(Some(sender));
+
+            let response_handler = self.connect_response(move |_, response_type| {
+                if let Some(m) = sender.replace(None) {
+                    let _result = m.send(response_type);
+                }
+            });
+
+            self.show();
+
+            if let Ok(response) = receiver.await {
+                if response != ResponseType::DeleteEvent {
+                    self.disconnect(response_handler);
+                }
+                response
+            } else {
+                ResponseType::None
+            }
+        })
     }
 }
