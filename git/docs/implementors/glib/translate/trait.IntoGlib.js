16ee0134ce1edb6c9372892793780efb67403a06
sdroege <sdroege@users.noreply.github.com>
2021-04-27 17:11:36 +0000

    deploy: f8255d141db8438bb385ab0bb0b2b5dad293e228

---
+++ git/docs/implementors/glib/translate/trait.IntoGlib.js
@@ -0,0 +1,4 @@
+(function() {var implementors = {};
+implementors["gdk"] = [{"text":"impl <a class=\"trait\" href=\"glib/translate/trait.IntoGlib.html\" title=\"trait glib::translate::IntoGlib\">IntoGlib</a> for <a class=\"struct\" href=\"gdk/keys/struct.Key.html\" title=\"struct gdk::keys::Key\">Key</a>","synthetic":false,"types":["gdk::keys::Key"]}];
+implementors["glib"] = [];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
