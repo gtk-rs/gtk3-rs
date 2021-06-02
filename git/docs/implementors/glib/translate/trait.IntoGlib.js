f12367588a44ea5eea4c1fc88c4b7a9909a64f54
bilelmoussaoui <bilelmoussaoui@users.noreply.github.com>
2021-06-02 09:43:16 +0000

    deploy: f53e57473d6c3d015ea65a6901bcc4cfa8fb5235

--- git/docs/implementors/glib/translate/trait.IntoGlib.js
+++ git/docs/implementors/glib/translate/trait.IntoGlib.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {};
-implementors["gdk"] = [{"text":"impl IntoGlib for <a class=\"struct\" href=\"gdk/keys/struct.Key.html\" title=\"struct gdk::keys::Key\">Key</a>","synthetic":false,"types":["gdk::keys::Key"]}];
+implementors["gdk"] = [{"text":"impl <a class=\"trait\" href=\"https://gtk-rs.org/gtk-rs-core/git/docs/glib/translate/trait.IntoGlib.html\" title=\"trait glib::translate::IntoGlib\">IntoGlib</a> for <a class=\"struct\" href=\"gdk/keys/struct.Key.html\" title=\"struct gdk::keys::Key\">Key</a>","synthetic":false,"types":["gdk::keys::Key"]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
