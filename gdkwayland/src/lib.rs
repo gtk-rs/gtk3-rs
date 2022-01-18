b2ee1879f9ef6001bdbeeff2f059b279ab100e9a
Bilal Elmoussaoui <bil.elmoussaoui@gmail.com>
2022-01-18 11:06:17 +0100

    CI: document the new gdkwayland crates (#719)
    * CI: document the new gdkwayland crates

--- gdkwayland/src/lib.rs
+++ gdkwayland/src/lib.rs
@@ -1,5 +1,6 @@
 // Take a look at the license at the top of the repository in the LICENSE file.
 
+#![cfg_attr(feature = "dox", feature(doc_cfg))]
 #![doc = include_str!("../README.md")]
 
 pub use ffi;
