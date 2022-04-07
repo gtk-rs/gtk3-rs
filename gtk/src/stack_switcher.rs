7b72259165afc0ae63554b7151134ccb9ecccca0
Sebastian Dröge <sebastian@centricular.com>
2022-04-07 11:50:57 +0300

    gtk: Fix new clippy warning
    ```
     error: this expression creates a reference which is immediately dereferenced by the compiler
      --> gtk/src/stack_switcher.rs:52:15
       |
    52 |             f(&StackSwitcher::from_glib_borrow(this).unsafe_cast_ref())
       |               ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ help: change this to: `StackSwitcher::from_glib_borrow(this).unsafe_cast_ref()`
       |
       = note: `-D clippy::needless-borrow` implied by `-D warnings`
       = help: for further information visit https://rust-lang.github.io/rust-clippy/master/index.html#needless_borrow
    ```

--- gtk/src/stack_switcher.rs
+++ gtk/src/stack_switcher.rs
@@ -65,7 +65,7 @@ impl<O: IsA<StackSwitcher>> StackSwitcherExtManual for O {
             f: glib::ffi::gpointer,
         ) {
             let f: &F = &*(f as *const F);
-            f(&StackSwitcher::from_glib_borrow(this).unsafe_cast_ref())
+            f(StackSwitcher::from_glib_borrow(this).unsafe_cast_ref())
         }
         unsafe {
             let f: Box_<F> = Box_::new(f);
