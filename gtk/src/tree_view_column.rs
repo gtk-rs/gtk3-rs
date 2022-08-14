53372dc4296a25df1b5d7177957dac17f0ffbe61
Bilal Elmoussaoui <belmouss@redhat.com>
2022-08-14 11:34:26 +0200

    gtk: Drop manual TreeViewColumn methods
    They are properly auto generated now

--- gtk/src/tree_view_column.rs
+++ gtk/src/tree_view_column.rs
@@ -2,7 +2,6 @@
 
 use crate::{prelude::TreeViewColumnExt, CellRenderer, TreeViewColumn};
 use glib::object::IsA;
-use glib::translate::*;
 
 impl TreeViewColumn {
     #[doc(alias = "gtk_tree_view_column_new_with_attributes")]
@@ -20,23 +19,6 @@ impl TreeViewColumn {
         tree_view_column
     }
 
-    #[doc(alias = "gtk_tree_view_column_add_attribute")]
-    pub fn add_attribute(
-        &self,
-        cell_renderer: &impl IsA<CellRenderer>,
-        attribute: &str,
-        column: i32,
-    ) {
-        unsafe {
-            ffi::gtk_tree_view_column_add_attribute(
-                self.to_glib_none().0,
-                cell_renderer.as_ref().to_glib_none().0,
-                attribute.to_glib_none().0,
-                column,
-            )
-        }
-    }
-
     #[doc(alias = "gtk_tree_view_column_set_attributes")]
     pub fn set_attributes(
         &self,
@@ -47,44 +29,5 @@ impl TreeViewColumn {
         attributes.iter().for_each(|(attribute, column)| {
             self.add_attribute(cell_renderer, attribute, *column);
         });
-    }
-
-    #[doc(alias = "gtk_tree_view_column_clear_attributes")]
-    pub fn clear_attributes(&self, cell_renderer: &impl IsA<CellRenderer>) {
-        unsafe {
-            ffi::gtk_tree_view_column_clear_attributes(
-                self.to_glib_none().0,
-                cell_renderer.as_ref().to_glib_none().0,
-            );
-        }
-    }
-
-    #[doc(alias = "gtk_tree_view_column_pack_end")]
-    pub fn pack_end(&self, cell_renderer: &impl IsA<CellRenderer>, expand: bool) {
-        unsafe {
-            ffi::gtk_tree_view_column_pack_end(
-                self.to_glib_none().0,
-                cell_renderer.as_ref().to_glib_none().0,
-                expand.into_glib(),
-            )
-        }
-    }
-
-    #[doc(alias = "gtk_tree_view_column_pack_start")]
-    pub fn pack_start(&self, cell_renderer: &impl IsA<CellRenderer>, expand: bool) {
-        unsafe {
-            ffi::gtk_tree_view_column_pack_start(
-                self.to_glib_none().0,
-                cell_renderer.as_ref().to_glib_none().0,
-                expand.into_glib(),
-            )
-        }
-    }
-
-    #[doc(alias = "gtk_tree_view_column_clear")]
-    pub fn clear(&self) {
-        unsafe {
-            ffi::gtk_tree_view_column_clear(self.to_glib_none().0);
-        }
     }
 }
