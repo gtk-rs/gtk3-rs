79bf90fa98697403d9d94b17ee68c5f594ba1f0a
Mițca Dumitru <dumitru0mitca@gmail.com>
2023-01-23 15:03:49 +0200

    examples: Adapt to glib-build-tools breaking change

--- examples/build.rs
+++ examples/build.rs
@@ -1,6 +1,6 @@
 fn main() {
     glib_build_tools::compile_resources(
-        "resources",
+        &["resources"],
         "resources/resources.gresource.xml",
         "compiled.gresource",
     );
