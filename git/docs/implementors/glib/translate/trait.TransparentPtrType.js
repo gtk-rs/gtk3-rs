f53356ac88c95a27219bc6afcad1efcfec6c7efb
sdroege <sdroege@users.noreply.github.com>
2023-03-23 15:39:28 +0000

    deploy: a74677d1d1f14e614c3f84b497fa32733e01080b

--- git/docs/implementors/glib/translate/trait.TransparentPtrType.js
+++ git/docs/implementors/glib/translate/trait.TransparentPtrType.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"gdk":[["impl <a class=\"trait\" href=\"https://gtk-rs.org/gtk-rs-core/git/docs/glib/translate/trait.TransparentPtrType.html\" title=\"trait glib::translate::TransparentPtrType\">TransparentPtrType</a> for <a class=\"struct\" href=\"gdk/struct.Atom.html\" title=\"struct gdk::Atom\">Atom</a>"]]
+"gdk":[["impl <a class=\"trait\" href=\"https://gtk-rs.org/gtk-rs-core/stable/0.17/docs/glib/translate/trait.TransparentPtrType.html\" title=\"trait glib::translate::TransparentPtrType\">TransparentPtrType</a> for <a class=\"struct\" href=\"gdk/struct.Atom.html\" title=\"struct gdk::Atom\">Atom</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
