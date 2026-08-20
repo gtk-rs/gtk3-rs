5a530a4964b581a83eea70125570cd76e6b88ce8
Brian J. Tarricone <brian@tarricone.org>
2026-08-20 04:35:16 -0700

    Make TreeModelFilter subclassable
    Copied from gtk4-rs and edited a decent amount.

--- gtk/src/subclass/mod.rs
+++ gtk/src/subclass/mod.rs
@@ -36,6 +36,7 @@ pub mod stack;
 pub mod toggle_button;
 pub mod tree_drag_dest;
 pub mod tree_drag_source;
+pub mod tree_model_filter;
 pub mod tree_model_sort;
 pub mod tree_store;
 pub mod tree_view;
@@ -82,6 +83,7 @@ pub mod prelude {
     pub use super::toggle_button::ToggleButtonImpl;
     pub use super::tree_drag_dest::{TreeDragDestImpl, TreeDragDestImplExt};
     pub use super::tree_drag_source::{TreeDragSourceImpl, TreeDragSourceImplExt};
+    pub use super::tree_model_filter::{TreeModelFilterImpl, TreeModelFilterImplExt};
     pub use super::tree_model_sort::TreeModelSortImpl;
     pub use super::tree_store::TreeStoreImpl;
     pub use super::tree_view::{TreeViewImpl, TreeViewImplExt};
